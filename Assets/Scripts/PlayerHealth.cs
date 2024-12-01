8a94dc6caab64a5b813e7ee4db652b2db2eeaa0c
Xuanharu <xuan12345.912.pch@gmail.com>
2024-12-01 11:02:41 +0700

    HealthBar

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerHealth : Health
+{
+    public Image healthBar; // Thay Slider bằng Image
+    public TextMeshProUGUI healthText; // TextMeshProUGUI
+
+    private void Start()
+    {
+        currentHP = maxHP;
+        UpdateHealthUI();
+    }
+
+    public override void TakeDamage(float damage)
+    {
+        base.TakeDamage(damage);
+        UpdateHealthUI();
+    }
+
+    private void UpdateHealthUI()
+    {
+        healthBar.fillAmount = currentHP / maxHP;
+        healthText.text = $"{currentHP:F0} / {maxHP:F0}"; // Hiển thị số máu
+    }
+}
+
+
