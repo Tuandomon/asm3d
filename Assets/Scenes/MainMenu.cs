4a440a3e6a640c4c7eef0ab573b9bdc65272357a
Xuanharu <xuan12345.912.pch@gmail.com>
2024-12-02 13:55:25 +0700

    giao diện

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scene9"); // Thay "GameScene" bằng tên scene của game
+    }
+
+    public void OpenOptions()
+    {
+        // Mở cửa sổ tùy chọn hoặc scene tùy chọn
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
