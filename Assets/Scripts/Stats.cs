3ac484e317610ae1a158a2bf39315069feaf94ed
Xuanharu <xuan12345.912.pch@gmail.com>
2024-12-03 19:59:50 +0700

    stat khong thanh

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class IncreaseHealth : MonoBehaviour
+{
+    public int health = 100; // Máu ban đầu của nhân vật
+    public Button increaseHealthButton; // Nút để tăng máu
+
+    void Start()
+    {
+        // Đảm bảo rằng nút được gắn đúng sự kiện
+        increaseHealthButton.onClick.AddListener(IncreaseHealthPoints);
+    }
+
+    void IncreaseHealthPoints()
+    {
+        health += 10; // Tăng máu lên 10 điểm mỗi lần bấm nút
+        Debug.Log("Health increased. Current health: " + health);
+    }
+}
